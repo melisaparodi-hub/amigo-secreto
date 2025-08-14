cb4b9d4a8eac1782572ce16ce2f26271ff6c3943
Melisa <melisaparodi.ar@gmail.com>
2025-08-13 22:14:38 -0300

    Actualiza estructura y botones en index.html

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ function agregarAmigo() {
 
   if (nombresValorInput != "") {
     amigos.push(nombresValorInput);
+    limpiarCaja();
   } else {
     alert("Por favor, inserte un nombre.");
   }
   console.log(amigos);
+  return;
+}
+
+function limpiarCaja() {
+  document.querySelector("input").value = " ";
+}
+
+document.querySelector("#listaAmigos");
+
+function limpiarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
 }
