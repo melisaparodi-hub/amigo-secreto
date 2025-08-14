53f4e710e6d626d9222df0fb2b228c6241f96070
Melisa <melisaparodi.ar@gmail.com>
2025-08-13 21:12:43 -0300

    agrega función para añadir nombres de amigos desde un input

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let nombresValorInput = document.getElementById("amigo").value;
+
+  if (nombresValorInput != "") {
+    amigos.push(nombresValorInput);
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
+  console.log(amigos);
+}
